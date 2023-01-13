7509db3297f0349257bea8f0a6365322b9515e78
SurajSingh45 <suraj.12012199@lpu.in>
2023-01-13 19:50:42 +0530

    questions

---
+++ index.js
@@ -0,0 +1,5 @@
+var a = "15";
+var b = 15;
+var c = b/a
+console.log(a==b)
+console.log(typeof c)
---
+++ question3.js
@@ -0,0 +1,6 @@
+/*Being a nonconformist, Volodya is displeased with the current state of things, particularly with the order of natural numbers
+ (natural number is positive integer number). He is determined to rearrange them. But there are too many natural numbers, so 
+ Volodya decided to start with the first n. He writes down the following sequence of numbers: firstly all odd integers from 
+ 1 to n (in ascending order), then all even integers from 1 to n (also in ascending order). Help our hero to find out which 
+ number will stand at the position number k.
+The only line of input contains integers n and k (1 ≤ k ≤ n ≤ 10^12).*/
